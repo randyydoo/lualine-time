17954ff19858cc75b9692b692f9acef6c258919e
randyydoo <randyydoo@gmail.com>
2024-02-17 20:28:28 -0800

    changed ui

--- lua/lualine/components/cdate.lua
+++ lua/lualine/components/cdate.lua
@@ -1,13 +1,13 @@
 -- Inspired by: https://github.com/arkav/lualine-lsp-progress
 
-local CTimeLine = require('lualine.component'):extend()
+local CTimeLine = require("lualine.component"):extend()
 
 CTimeLine.init = function(self, options)
 	CTimeLine.super.init(self, options)
 end
 
 CTimeLine.update_status = function(self)
-    return os.date("%Y-%m-%d", os.time())
+	return os.date("%m-%d-%Y", os.time())
 end
 
 return CTimeLine
